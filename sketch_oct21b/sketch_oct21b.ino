da4f34df0b967f2f996fb0ed1cdea180f7890403
German Chaparro <germancho@gmail.com>
2016-10-24 16:54:59 -0500

    azimuth sketch

--- sketch_oct21b/sketch_oct21b.ino
+++ sketch_oct21b/sketch_oct21b.ino
@@ -19,25 +19,44 @@ int potPin = 3;    // select the input pin for the potentiometer
 int ledPin = 5;   // select the pin for the LED
 int ledPin2 = 6;   // select the pin for the LED
 
-int val = 0;       // variable to store the value coming from the sensor
+float val = 0;       // variable to store the value coming from the sensor
 int valcont = 0;
 int valold = 0;
+float vref = 2.5;
+float eps = 0.1;
 
 void setup() {
   pinMode(ledPin, OUTPUT);  // declare the ledPin as an OUTPUT
   pinMode(ledPin2, OUTPUT);  // declare the ledPin as an OUTPUT
-
+  digitalWrite(ledPin, HIGH);  // turn the ledPin on
+  digitalWrite(ledPin2, HIGH);  // turn the ledPin on
   Serial.begin(9600);
 }
 
 void loop() {
-  val = analogRead(potPin);    // read the value from the sensor
-  valcont = (int)((float)val*5./1023.);
+ 
+  val = (float)analogRead(potPin)*5./1024.;
+//((int)(val*10))/10.;
+  if (val < vref - eps){
+    valcont = 5;
+  }
+  if (val > vref + eps){
+    valcont = 3;
+  }
+  if (val >= vref - eps && val <= vref + eps){
+    valcont = 0;
+  }  
+  
+//(int)((float)val*5./1023.);
   if( valold != valcont && valcont != 0 ){
-     delay(1500);
+    delay(1500);
+    digitalWrite(ledPin, HIGH);  // turn the ledPin on
+    digitalWrite(ledPin2, HIGH);  // turn the ledPin on
   }
   valold = valcont;
-  Serial.print((float)val*5./1023.);
+  Serial.print(val);
+  Serial.print("\t");
+  Serial.print(analogRead(potPin));
   Serial.print("\t");
   Serial.print(valcont);
   Serial.print("\n");
